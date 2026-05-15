d99f3195350029708baf5d59110baa9da3671017
andeeboyy <andertu120@gmail.com>
2026-05-15 16:29:38 -0700

    Update weapon_knife.lua

--- weapons/weapon_knife.lua
+++ weapons/weapon_knife.lua
@@ -41,16 +41,49 @@ end
 -- shoot
 function SWEP:PrimaryAttack()
     local bullet = {}
-	bullet.Damage = 7
-	bullet.Num = 10
+	bullet.Damage = 5
+	bullet.Num = 3
 	bullet.Force = 0.05
-	bullet.Distance = 65
+	bullet.Distance = 30
 	bullet.Dir = self:GetOwner():GetAimVector()
 	bullet.Src = self:GetOwner():GetShootPos()
 	bullet.Spread = Vector(0.4, 0.05, 0.4)
 	bullet.TracerName = "none"
 	bullet.Tracer = 0
-    self:FireBullets(bullet)
+    timer.Simple(0.075, function()
+    	self:FireBullets(bullet)
+	bullet.Distance = 40
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+    end)
+    timer.Simple(0.1, function()
+    	self:FireBullets(bullet)
+	bullet.Distance = 50
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+    end)
+    timer.Simple(0.125, function()
+    	self:FireBullets(bullet)
+	bullet.Distance = 65
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+    end)
+    timer.Simple(0.15, function()
+    	self:FireBullets(bullet)
+	bullet.Distance = 50
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+    end)
+    timer.Simple(0.175, function()
+    	self:FireBullets(bullet)
+	bullet.Distance = 40
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+    end)
+    timer.Simple(0.2, function()
+    	self:FireBullets(bullet)
+	bullet.Distance = 30
+    end)
     self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
     self:GetOwner():SetAnimation(PLAYER_ATTACK1)
     self:EmitSound("npc/vort/claw_swing2.wav", 120, 100, 1, CHAN_AUTO)
@@ -60,30 +93,75 @@ end
 
 function SWEP:SecondaryAttack()
     local bullet = {}
-	bullet.Damage = 6
-	bullet.Num = 10
+	bullet.Damage = 2
+	bullet.Num = 3
 	bullet.Force = 0.1
-	bullet.Distance = 75
+	bullet.Distance = 45
 	bullet.Dir = self:GetOwner():GetAimVector()
 	bullet.Src = self:GetOwner():GetShootPos()
 	bullet.Spread = Vector(0.1, 0.05, 0.1)
 	bullet.TracerName = "none"
 	bullet.Tracer = 0
-    self:FireBullets(bullet)
+    timer.Simple(0.05, function()
+    	self:FireBullets(bullet)
+	bullet.Distance = 65
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+    end)
+    timer.Simple(0.06, function()
+    	self:FireBullets(bullet)
+	bullet.Distance = 75
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+    end)
+    timer.Simple(0.07, function()
+    	self:FireBullets(bullet)
+	bullet.Distance = 76
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+    end)
+    timer.Simple(0.08, function()
+    	self:FireBullets(bullet)
+	bullet.Distance = 77
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+    end)
+    timer.Simple(0.09, function()
+    	self:FireBullets(bullet)
+	bullet.Distance = 78
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+	bullet.Damage = 4
+	bullet.Num = 1
+    end)
+    timer.Simple(0.1, function()
+    	self:FireBullets(bullet)
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+	bullet.Distance = 79
+    end)
+    timer.Simple(0.2, function()
+    	self:FireBullets(bullet)
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+	bullet.Distance = 80
+    end)
+    timer.Simple(0.3, function()
+    	self:FireBullets(bullet)
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+    end)
+    timer.Simple(0.4, function()
+    	self:FireBullets(bullet)
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+    end)
+    timer.Simple(0.5, function()
+    	self:FireBullets(bullet)
+    end)
     self:SendWeaponAnim(ACT_VM_SECONDARYATTACK)
     self:GetOwner():SetAnimation(PLAYER_ATTACK1)
     self:EmitSound("weapons/iceaxe/iceaxe_swing1.wav", 120, 70, 1, CHAN_AUTO)
     self:SetNextSecondaryFire(CurTime() + 0.8)
     self:SetNextPrimaryFire(CurTime() + 0.8)
-end
-function SWEP:Deploy()
-    self:GetOwner():SetWalkSpeed(220)
-    self:GetOwner():SetRunSpeed(420)
-    return true
-end
-
-function SWEP:Holster()
-    self:GetOwner():SetWalkSpeed(200)
-    self:GetOwner():SetRunSpeed(400)
-    return true
 end
