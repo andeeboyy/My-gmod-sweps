803adbb66e188e258af131426edf56e8f3cb82b7
andeeboyy <andertu120@gmail.com>
2026-05-24 18:08:03 -0700

    Add files via upload

---
+++ weapons/weapon_p228.lua
@@ -0,0 +1,77 @@
+AddCSLuaFile()
+local AccuracyMeter = 0
+-- spawnmenu
+
+SWEP.Spawnable = true
+SWEP.PrintName = "Sig Sauer P228"
+SWEP.Base = "weapon_base"
+SWEP.Category = "my guns"
+
+-- viewmodel
+
+SWEP.ViewModel = "models/weapons/cstrike/c_pist_p228.mdl"
+SWEP.WorldModel = "models/weapons/w_pist_p228.mdl"
+SWEP.UseHands = true
+SWEP.ViewModelFov = 50
+
+-- slots
+
+SWEP.SlotPos = 3
+SWEP.Slot = 1
+
+-- stats
+
+SWEP.AccurateCrossHair = true
+SWEP.Primary.Ammo = "pistol"
+SWEP.Primary.ClipSize = 13
+SWEP.Primary.DefaultClip = 13
+SWEP.Primary.Automatic = false
+
+-- secondary
+
+SWEP.Secondary.ClipSize    = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic   = false
+SWEP.Secondary.Ammo        = "none"
+
+-- function stuff
+
+-- anim
+function SWEP:Initialize()
+    self:SetHoldType("Pistol")
+end
+
+
+-- shoot
+function SWEP:PrimaryAttack()
+    if ( !self:CanPrimaryAttack() ) then return end
+    self:GetOwner():ViewPunch( Angle( AccuracyMeter * -16, math.random(AccuracyMeter * 16, AccuracyMeter * -16), 0 ) )
+    local bullet = {}
+	bullet.Damage = 11
+	bullet.Num = 1
+	bullet.Force = 2.33
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+	bullet.Spread = Vector(AccuracyMeter, AccuracyMeter, AccuracyMeter)
+	AccuracyMeter = AccuracyMeter + 0.18
+    self:FireBullets(bullet)
+    self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+    self:GetOwner():SetAnimation(PLAYER_ATTACK1)
+    self:EmitSound("weapons/alyx_gun/alyx_gun_fire4.wav", 140, 75, 1, CHAN_WEAPON)
+    self:SetNextPrimaryFire(CurTime() + 0.15)
+    self:TakePrimaryAmmo(1)
+end
+
+-- reload the magazine to shoot more bullets
+
+function SWEP:Reload()
+    self:DefaultReload(ACT_VM_RELOAD)
+    self:GetOwner():SetAnimation(ACT_RELOAD)
+end
+
+function SWEP:Think()
+    if AccuracyMeter > 0.005 then
+	AccuracyMeter = math.max(0, AccuracyMeter - (0.8 * FrameTime()))
+    end
+end
+
