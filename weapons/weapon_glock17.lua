9eb71877409345564d760fb129fe04973fd8d73e
andeeboyy <andertu120@gmail.com>
2026-06-12 19:28:35 -0700

    Add files via upload

---
+++ weapons/weapon_glock17.lua
@@ -0,0 +1,80 @@
+AddCSLuaFile()
+local AccuracyMeter = 0
+-- spawnmenu
+
+SWEP.Spawnable = true
+SWEP.PrintName = "Glock-17"
+SWEP.Base = "weapon_base"
+SWEP.Category = "my guns"
+
+-- viewmodel
+
+SWEP.ViewModel = "models/weapons/cstrike/c_pist_glock18.mdl"
+SWEP.WorldModel = "models/weapons/w_pist_glock18.mdl"
+SWEP.UseHands = true
+SWEP.ViewModelFov = 50
+
+-- slots
+
+SWEP.SlotPos = 3
+SWEP.Slot = 1
+
+-- stats
+
+SWEP.AccurateCrossHair = true
+SWEP.Primary.Ammo = "pistol"
+SWEP.Primary.ClipSize = 17
+SWEP.Primary.DefaultClip = 17
+SWEP.Primary.Automatic = false
+
+-- secondary
+
+SWEP.Secondary.ClipSize    = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic   = false
+SWEP.Secondary.Ammo        = "none"
+
+-- function stuff
+
+-- anim
+function SWEP:Initialize()
+    self:SetHoldType("Pistol")
+end
+
+
+-- shoot
+function SWEP:PrimaryAttack()
+    if ( !self:CanPrimaryAttack() ) then return end
+    if !self:GetOwner():IsNPC() then
+    	self:GetOwner():ViewPunch( Angle( AccuracyMeter * -16, math.random(AccuracyMeter * 16, AccuracyMeter * -16), 0 ) )
+    end
+    local bullet = {}
+	bullet.Damage = 8
+	bullet.Num = 1
+	bullet.Force = 2
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+	bullet.Spread = Vector(AccuracyMeter, AccuracyMeter, AccuracyMeter)
+	AccuracyMeter = AccuracyMeter + 0.05
+    self:FireBullets(bullet)
+    self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+    self:GetOwner():SetAnimation(PLAYER_ATTACK1)
+    EmitSound("weapons/smg1/smg1_fire1.wav", self:GetPos(), 0, CHAN_WEAPON, 1, 140, 0, 90)
+    self:EmitSound("weapons/ar2/fire1.wav", 140, 150, 1, CHAN_WEAPON)
+    self:SetNextPrimaryFire(CurTime() + 0.08)
+    self:TakePrimaryAmmo(1)
+end
+
+-- reload the magazine to shoot more bullets
+
+function SWEP:Reload()
+    self:DefaultReload(ACT_VM_RELOAD)
+    self:GetOwner():SetAnimation(ACT_RELOAD)
+end
+
+function SWEP:Think()
+    if AccuracyMeter > 0.01 then
+	AccuracyMeter = math.max(0, AccuracyMeter - (0.25 * FrameTime()))
+    end
+end
+
---
+++ weapons/weapon_usp_45.lua
@@ -0,0 +1,80 @@
+AddCSLuaFile()
+local AccuracyMeter = 0
+-- spawnmenu
+
+SWEP.Spawnable = true
+SWEP.PrintName = "USP 45"
+SWEP.Base = "weapon_base"
+SWEP.Category = "my guns"
+
+-- viewmodel
+
+SWEP.ViewModel = "models/weapons/cstrike/c_pist_usp.mdl"
+SWEP.WorldModel = "models/weapons/w_pist_usp.mdl"
+SWEP.UseHands = true
+SWEP.ViewModelFov = 50
+
+-- slots
+
+SWEP.SlotPos = 3
+SWEP.Slot = 1
+
+-- stats
+
+SWEP.AccurateCrossHair = true
+SWEP.Primary.Ammo = "pistol"
+SWEP.Primary.ClipSize = 12
+SWEP.Primary.DefaultClip = 12
+SWEP.Primary.Automatic = false
+
+-- secondary
+
+SWEP.Secondary.ClipSize    = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic   = false
+SWEP.Secondary.Ammo        = "none"
+
+-- function stuff
+
+-- anim
+function SWEP:Initialize()
+    self:SetHoldType("Pistol")
+end
+
+
+-- shoot
+function SWEP:PrimaryAttack()
+    if ( !self:CanPrimaryAttack() ) then return end
+    if !self:GetOwner():IsNPC() then
+    	self:GetOwner():ViewPunch( Angle( AccuracyMeter * -16, math.random(AccuracyMeter * 16, AccuracyMeter * -16), 0 ) )
+    end
+    local bullet = {}
+	bullet.Damage = 10
+	bullet.Num = 1
+	bullet.Force = 3
+	bullet.Dir = self:GetOwner():GetAimVector()
+	bullet.Src = self:GetOwner():GetShootPos()
+	bullet.Spread = Vector(AccuracyMeter, AccuracyMeter, AccuracyMeter)
+	AccuracyMeter = AccuracyMeter + 0.065
+    self:FireBullets(bullet)
+    self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+    self:GetOwner():SetAnimation(PLAYER_ATTACK1)
+    EmitSound("weapons/357/357_fire2.wav", self:GetPos(), 0, CHAN_WEAPON, 1, 140, 0, 110)
+    self:EmitSound("weapons/pistol/pistol_fire2.wav", 140, 75, 1, CHAN_WEAPON)
+    self:SetNextPrimaryFire(CurTime() + 0.1)
+    self:TakePrimaryAmmo(1)
+end
+
+-- reload the magazine to shoot more bullets
+
+function SWEP:Reload()
+    self:DefaultReload(ACT_VM_RELOAD)
+    self:GetOwner():SetAnimation(ACT_RELOAD)
+end
+
+function SWEP:Think()
+    if AccuracyMeter > 0.025 then
+	AccuracyMeter = math.max(0, AccuracyMeter - (0.3 * FrameTime()))
+    end
+end
+
